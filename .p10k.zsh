cfb49e9ff73ff6d1f42e07e832041db3a6daac4a
Joseph Lozano <me@lozanojoseph.com>
2023-10-19 14:33:09 -0400

    update delimiter

--- .p10k.zsh
+++ .p10k.zsh
@@ -221,7 +221,7 @@
   # prefix. The shortened directory can be tab-completed to the original.
   typeset -g POWERLEVEL9K_SHORTEN_STRATEGY=truncate_from_right
   # Replace removed segment suffixes with this symbol.
-  typeset -g POWERLEVEL9K_SHORTEN_DELIMITER=…
+  typeset -g POWERLEVEL9K_SHORTEN_DELIMITER=
   # Color of the shortened directory segments.
   typeset -g POWERLEVEL9K_DIR_SHORTENED_FOREGROUND=250
   # Color of the anchor directory segments. Anchor segments are never shortened. The first
